591cc32d1c968e98fcc9cb10ad7bc51558bd4159
orsaskope <gkokkinis@protonmail.com>
2021-11-13 21:15:50 +0200

    add entry

--- Project1/modules/entries/entry.c
+++ Project1/modules/entries/entry.c
@@ -1,19 +1,68 @@
 #include"../../include/entry.h"
 
-Entry entry_create(String word){
+void create_entry(const String word, Entry entry){
     // Allocate memory for the entry
-    Entry entry=malloc(sizeof(*entry));
+    entry = (Entry)malloc(sizeof(entry));
     // Set word
-    entry->word=strdup(word);
+    entry->word = strdup(word);
     // Set payload to NULL for Project1
-    entry->payload=NULL;
-    // Return the entry
-    return entry;
+    entry->payload = NULL;
+    // Set next to NULL (entries also function as Entry List nodes)
+    entry->next = NULL;
 }
 
-void entry_destroy(Entry entry){
+void destroy_entry(Entry entry){
     // Free the word
     free(entry->word);
     // Free the entry
     free(entry);
+}
+
+void create_entry_list(EntryList el){
+    
+    el = (EntryList)malloc(sizeof(entry_list));
+    
+    el->head = NULL;
+    el->end = NULL;
+
+}
+
+unsigned int get_number_entries(const EntryList el){
+
+    Entry temp = el->head;
+
+    unsigned int number = 0;
+
+    while(temp!=NULL){
+        number++;
+        temp = temp->next;
+    }
+
+    return number;
+
+}
+
+void add_entry(EntryList el, const Entry e){
+    el->end->next = e;
+}
+
+Entry get_first(const EntryList el){
+    if(el->head!=NULL)  return el->head;
+    else return NULL;
+}
+
+Entry get_next(const EntryList el, const Entry e){
+    if(e->next!=NULL) return e->next;
+    else return NULL;
+}
+
+void destroy_list_nodes(Entry e){
+    if(e==NULL) return;
+    destroy_list_nodes(e->next);
+    destroy_entry(e);
+}
+
+void destroy_entry_list(EntryList el){
+    destroy_list_nodes(get_first(el));
+    free(el);
 }
