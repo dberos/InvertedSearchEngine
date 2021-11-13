fa0cdac5672f0465310cb3b1397f87658ba18e4e
orsaskope <sdi1900341@di.uoa.gr>
2021-11-13 21:06:43 +0200

    add entry

--- Project1/include/entry.h
+++ Project1/include/entry.h
@@ -5,10 +5,38 @@
 struct entry{
     String word;
     String payload;
-};
+    Entry next;
+} entry;
+
+
+struct entry_list{
+    Entry head;
+    Entry end;
+} entry_list;
 
 // Creating an Entry
-Entry entry_create(String word);
+void create_entry(String word, Entry entry);
 
 // Destroying an Entry
-void entry_destroy(Entry entry);
+void destroy_entry(Entry entry);
+
+//Creating and entry list
+void create_entry_list(EntryList el);
+
+//Count length of entry list
+unsigned int get_number_entries(const EntryList el);
+
+//Add entry to entry lsit
+void add_entry(EntryList el, const Entry e);
+
+//Get first entry of the entrylist
+Entry get_first(const EntryList el);
+
+//Get next entry of the list(given the previous entry)
+Entry get_next(const EntryList el, const Entry e);
+
+//Destroy entry list
+void destroy_list_nodes(Entry e); //Recursive function used in destroy_entry_list
+void destroy_entry_list(EntryList el);
+
+
