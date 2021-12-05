105f6fa14bcf05c958975f224af80a957a37fc27
dberos <sdi1600269@di.uoa.gr>
2021-12-05 20:30:14 +0200

    add SIGMOD files and changed Makefile to create core.so

---
+++ Project2/modules/methods/core.c
@@ -0,0 +1,95 @@
+/*
+ * core.cpp version 1.0
+ * Copyright (c) 2013 KAUST - InfoCloud Group (All Rights Reserved)
+ * Author: Amin Allam
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include"../../include/common_types.h"
+#include"../../include/core.h"
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+// Computes edit distance between a null-terminated string "a" with length "na"
+//  and a null-terminated string "b" with length "nb" 
+// int EditDistance(char* a, int na, char* b, int nb)
+// {
+// 	return EC_SUCCESS;
+// }
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+// Computes Hamming distance between a null-terminated string "a" with length "na"
+//  and a null-terminated string "b" with length "nb" 
+// unsigned int HammingDistance(char* a, int na, char* b, int nb)
+// {
+// 	return EC_SUCCESS;
+// }
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+// Keeps all information related to an active query
+struct Query
+{
+	QueryID query_id;
+	char str[MAX_QUERY_LENGTH];
+	MatchType match_type;
+	unsigned int match_dist;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+// Keeps all query ID results associated with a dcoument
+struct Document
+{
+	DocID doc_id;
+	unsigned int num_res;
+	QueryID* query_ids;
+};
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+ErrorCode InitializeIndex(){return EC_SUCCESS;}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+ErrorCode DestroyIndex(){return EC_SUCCESS;}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+ErrorCode StartQuery(QueryID query_id, const char* query_str, MatchType match_type, unsigned int match_dist){return EC_SUCCESS;}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+ErrorCode EndQuery(QueryID query_id){return EC_SUCCESS;}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+ErrorCode MatchDocument(DocID doc_id, const char* doc_str){return EC_SUCCESS;}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+ErrorCode GetNextAvailRes(DocID* p_doc_id, unsigned int* p_num_res, QueryID** p_query_ids){return EC_SUCCESS;}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
